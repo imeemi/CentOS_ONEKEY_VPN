897e03e5f0ef26800feff5f3bb599c70d363196a
kanmenshu <kanmenshu@gmail.com>
2017-04-06 13:30:16 +0800

    Update del.sh

--- del.sh
+++ del.sh
@@ -3,10 +3,13 @@ cat > /etc/shadowsocks.json<<-EOF
 {
     "server":"0.0.0.0",
     "server_port":9000,
+    "local_address":"127.0.0.1",
     "local_port":1080,
     "password":"asdf1234",
+    "timeout":300,
     "method":"aes-256-cfb",
-    "timeout":600
+    "fast_open":false
 }
 EOF
-/etc/init.d/shadowsocks restart
+/etc/init.d/shadowsocks stop
+/etc/init.d/shadowsocks start
